13696f4e2fbc0b06860633dd04c1198a7d85c073
Ezequiel <ezequielezequiel9@gmail.com>
2024-08-12 16:32:24 -0300

    edit verceljson

--- index.ts
+++ index.ts
@@ -61,20 +61,13 @@ app.use(
   })
 );
 
-//
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
-});
+///
+//////
+///
+/////////
+///
+///
+///
 
 app.listen(port, () => {
   console.log("Escuchando en el puerto:", port);
